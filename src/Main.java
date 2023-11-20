d34d10fc8bbb9e7e33b9e55723f67341b45a05c6
seokmin <msm0748@gmail.com>
2023-11-20 15:39:38 +0900

    study: 메인 클래스와 메인 메서드

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// 메인 클래스 (시작 클래스) 정의
+// - main 메서드 가짐
+// - 메인 클래스는 한 개! (Node에서 진입점 파일 역할)
 public class Main {
+    // 주석
+    //    1. // 한 줄
+    //    2. /* 여러 줄 */
+    //    3. /** 문서 주석 */
+
+    //    main 메소드
+    //    - 자바 응용 프로그램의 실행 시작점
+    //    - 애플리케이션이 실행되려면 최소 1개 존재해야 함
+    //    - JVM 은 세미콜론(;)으로 끝나는 문장을 하나의 명령문으로 인식
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Java Study Start! " + "wow");
+        System.out.println("Yeah!");
+        System.out.print("Hello and welcome!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
